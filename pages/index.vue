dcaf68c0cc2ed99becc9a10b7bfe0227cd1920bb
Matthew Doonan <mdo79@uclive.ac.nz>
2024-04-22 17:50:05 +1200

    got it to work

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,3 @@
-<script setup lang="ts">
-</script>
-
 <template>
   <section>
     <video ref="video" autoplay></video>
@@ -8,7 +5,17 @@
   </section>
 </template>
 
+<script setup lang="ts">
+import axios from "axios"; // Import Axios
+import fs from "fs"; // Import fs
+
+</script>
+
 <script lang="ts">
+
+
+import axios from "axios"; // Import Axios
+
 export default {
   data() {
     return {
@@ -30,22 +37,17 @@ export default {
       canvas.height = this.$refs.video.videoHeight;
       const ctx = canvas.getContext('2d');
       if (ctx) {
-        ctx.drawImage(this.$refs.video, 0, 0, canvas.width, canvas.height); // Corrected line
+        ctx.drawImage(this.$refs.video, 0, 0, canvas.width, canvas.height);
         const imageData = canvas.toDataURL('image/jpg');
-        const axios = require("axios");
-        const fs = require("fs");
 
-        const image = fs.readFileSync("YOUR_IMAGE.jpg", {
-          encoding: "base64"
-        });
-
+        // Make Axios request
         axios({
           method: "POST",
           url: "https://detect.roboflow.com/playing-cards-ow27d/4",
           params: {
             api_key: "cCUXoekH1Wr2R3DEbB4W"
           },
-          data: image,
+          data: imageData,
           headers: {
             "Content-Type": "application/x-www-form-urlencoded"
           }
