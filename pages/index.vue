191140aae8177fa23ccd82bb9f584aa249a440e2
Matthew Doonan <mdo79@uclive.ac.nz>
2024-04-25 12:48:27 +1200

    Implemented potential hands

--- pages/index.vue
+++ pages/index.vue
@@ -5,19 +5,22 @@
       <div class="row">
         <div class="col-lg-4 col-12">
           <h3 class="mt-5">
-            {{ currentCards.length > 1 ? 'Current Hand' : (currentCards.length == 1 ? 'Need 2 cards' : 'Draw two cards') }}
+            {{ currentCards.length > 1 ? 'Current Hand' : (currentCards.length == 1 ? 'Need 2 cards' : 'Show Hand to start') }}
           </h3>
           <div class="show-cards">
             <NuxtImg :src="'/cards/' + card + '.png'" v-for="card in currentCards.slice(0, 2)"/>
           </div>
-          <h4 class="mt-3 mb-1">
-            {{ currentCards.length > 1 ? 'Chance of Winning: ' + chanceOfWinning : ''}}
+          <h4 v-if="handStrength" class="mt-3 mb-1">
+            {{ 'Current hand strength: ' + handStrength}}
           </h4>
-          <h4>
-            {{ currentCards.length > 1 ? 'Hand Ranking: ' + 0 + '/169': ''}}
+          <h4 v-if="potentialHands.length > 0" class="mt-2 mb-1">
+            Potential hands:
           </h4>
+          <h5 v-for="pot in potentialHands" >
+            {{pot}}
+          </h5>
           <h3 class="mt-4">
-            {{ currentCards.length > 1 ? (currentCards.length <= 5 ? 'Show draw for predictions' : 'Draw') : '' }}
+            {{ currentCards.length > 2 ? 'Shared cards' : '' }}
           </h3>
           <div class="show-cards">
             <NuxtImg :src="'/cards/' + card + '.png'" v-for="card in currentCards.slice(2, 7)"/>
@@ -47,7 +50,8 @@ export default {
       clearTimes: 0,
       cardTimes: 0,
       previousList: [],
-      chanceOfWinning: 0,
+      handStrength: null,
+      potentialHands: [],
     }
   },
   mounted() {
@@ -104,7 +108,7 @@ export default {
         let ahead = 0;
         let tied = 0;
         let behind = 0;
-        const ourRank = this.calcHandRanking(savedCurrentCards);
+        const ourRank = this.calcHandRanking(savedCurrentCards, true);
         const fullDeck = ['10C', '10D', '10H', '10S', '2C', '2D', '2H', '2S', '3C', '3D', '3H', '3S', '4C', '4D', '4H', '4S', '5C', '5D', '5H', '5S', '6C', '6D', '6H', '6S', '7C', '7D', '7H', '7S', '8C', '8D', '8H', '8S', '9C', '9D', '9H', '9S', 'AC', 'AD', 'AH', 'AS', 'JC', 'JD', 'JH', 'JS', 'KC', 'KD', 'KH', 'KS', 'QC', 'QD', 'QH', 'QS']
         for (let card of savedCurrentCards) {
           let indexToRemove = fullDeck.indexOf(card);
@@ -118,7 +122,7 @@ export default {
         }
         for (let combination of combinations) {
           const possibility = withoutHand.concat(combination);
-          const oppRank = this.calcHandRanking(possibility);
+          const oppRank = this.calcHandRanking(possibility, false);
           if (ourRank > oppRank) {
             ahead++
           } else if (ourRank === oppRank) {
@@ -127,17 +131,43 @@ export default {
             behind++
           }
         }
-        const value = (ahead + tied / 2) / (ahead + tied + behind)
-        this.chanceOfWinning = value
+        const value = ((ahead + tied / 2) / (ahead + tied + behind)) * 100
+        console.log(ahead)
+        console.log(tied)
+        console.log(behind)
+        console.log(value)
+        console.log('')
+        if (90 <= value) {
+          this.handStrength = "Very Strong"
+        } else if (80 <= value && value < 90) {
+          this.handStrength = "Strong"
+        } else if (70 <= value && value < 80) {
+          this.handStrength = "Solid"
+        } else if (60 <= value && value < 70) {
+          this.handStrength = "Above average"
+        } else if (40 <= value && value < 60) {
+          this.handStrength = "Average"
+        } else if (30 <= value && value < 40) {
+          this.handStrength = "Below Average"
+        } else if (20 <= value && value < 30) {
+          this.handStrength = "Weak"
+        } else if (10 <= value && value < 20) {
+          this.handStrength = "Extremely Weak"
+        } else if (0 <= value && value < 10) {
+          this.handStrength = "Just Fold"
+        }
       }
     },
-    calcHandRanking(cards:string[]) {
-      const pairVal = this.checkPairing(cards);
-      const straightFlushValue = this.checkStraightFlush(cards);
+    calcHandRanking(cards:string[], isOwnHand: boolean) {
+      if (isOwnHand) {
+        this.potentialHands = []
+      }
+      const pairVal = this.checkPairing(cards, isOwnHand);
+      const straightFlushValue = this.checkStraightFlush(cards, isOwnHand);
       return Math.max(pairVal, straightFlushValue);
     },
     //Checks for any straights or flushes or both
-    checkStraightFlush(cards:string[]) {
+    checkStraightFlush(cards:string[], isOwnHand: boolean) {
       let ordering = []
       const suiteCount: { [key: string]: number } = {'C': 0, 'S': 0, 'H': 0, 'D': 0};
       for (let card of cards) {
@@ -149,22 +179,57 @@ export default {
       ordering = ordering.sort((a, b) => a - b);
       let isFlush = null;
       let inARow = [];
+      let potentialInARow = [];
+      let skipped = 0
       for (let i = 1; i < ordering.length; i++) {
         if (ordering[i] === ordering[i - 1] + 1) {
           if (inARow.length === 0) {
             inARow.push(ordering[i - 1])
             inARow.push(ordering[i])
+            potentialInARow.push(ordering[i - 1])
+            potentialInARow.push(ordering[i])
+          } else if (inARow.length >= 5) {
+            inARow.splice(0, 1)
+            inARow.push(ordering[i])
           } else  {
             inARow.push(ordering[i])
+            potentialInARow.push(ordering[i])
           }
         } else if (ordering[i] !== ordering[i - 1]){
           inARow = [];
+          let addedPot = false
+          if (isOwnHand && cards.length > 4) {
+            for (let y = 1; y <= (7 - cards.length - skipped); y++) {
+              if (ordering[i] === ordering[i - 1] + 1 + y) {
+                addedPot = true;
+                skipped++;
+                if (potentialInARow.length === 0) {
+                  potentialInARow.push(ordering[i - 1]);
+                  potentialInARow.push(ordering[i]);
+                } else {
+                  potentialInARow.push(ordering[i]);
+                }
+              }
+              if (!addedPot && potentialInARow.length + 7 - cards.length < 5 ) {
+                potentialInARow = [];
+                skipped = 0;
+                y = 1;
+              }
+            }
+          }
         }
       }
+      let hasAddedPotentialFlush = false
       for (let i of ['H', 'D', 'S', 'C']) {
         if (suiteCount[i] >= 5) {
           isFlush = i;
+        } else if (7 - cards.length + suiteCount[i] >= 5 && isOwnHand && cards.length > 2 && !hasAddedPotentialFlush) {
+          this.potentialHands.push("Flush")
+          hasAddedPotentialFlush = true
         }
+      }
+      if (7 - cards.length + potentialInARow.length >= 5 && isOwnHand && cards.length > 2) {
+        this.potentialHands.push("Straight")
       }
       if (inARow.length === 5 && isFlush) {
         let numInRow = 0
@@ -192,7 +257,7 @@ export default {
       return 0
     },
     /* Checks for high card, pair, two pair, three of a kind, full house and four of a kind **/
-    checkPairing(cards:string[]) {
+    checkPairing(cards:string[], isOwnHand:boolean) {
       let maxCardInHand = 0
       const numCounts: { [key: number]: number } = {};
       for (let card of cards) {
@@ -219,10 +284,22 @@ export default {
       } else if (maxNumber >= 3 && secondMax >= 2) {
         return maxCardInHand + (13 * 6)
       } else if (maxNumber === 3) {
+        if (7 - cards.length + 3 >= 4 && isOwnHand) {
+          this.potentialHands.push("Four of a kind")
+        }
+        if (7 - cards.length + 3 >= 5 && isOwnHand) {
+          this.potentialHands.push("Full House")
+        }
         return maxCardInHand + (13 * 3)
       } else if (maxNumber === 2 && secondMax === 2) {
         return maxCardInHand + (13 * 2)
       } else if (maxNumber === 2) {
+        if (7 - cards.length + 2 >= 3 && isOwnHand) {
+          this.potentialHands.push("Three of a kind")
+        }
+        if (7 - cards.length + 2 >= 3 && isOwnHand) {
+          this.potentialHands.push("Two pair")
+        }
         return maxCardInHand + 13
       }
       return maxCardInHand
@@ -236,7 +313,8 @@ export default {
       }
       if (this.clearTimes === 20) {
         this.currentCards = []
-        this.chanceOfWinning = 0;
+        this.handStrength = null
+        this.potentialHands = []
         this.cardTimes = 0
       }
     },
